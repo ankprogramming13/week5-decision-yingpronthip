950c059be2da778a5be31fbe62f075e71f07fe9d
Unknown <pronthipza@gmail.com>
2017-12-01 13:59:21 +0700

    ข้อ4

--- lab_5_4.cpp
+++ lab_5_4.cpp
@@ -11,6 +11,15 @@ int main() {
 
     switch (menu) {
         case 1: cout << "Call Program 1";
+             break;
+        case 2: cout << "Call Program 2";
+             break;
+        case 3: cout << "Call Program 3";
+             break;
+        case 4: cout << "Call Program 4";
+             break;
+        case 5: cout << "Call Program 5";
+        default: cout << " Error ";
     }
 
     return 0;
